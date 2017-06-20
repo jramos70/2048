35b472d952cd7cf620ae46dedec7a301e304b25d
jramos70 <jerome_ramos@brown.edu>
2017-06-20 09:14:27 -0400

    refactored code

--- board.py
+++ board.py
@@ -1,15 +1,15 @@
 from random import randint
-import getch
-import numpy as np
 from copy import deepcopy
 import random
+import getch
+import numpy as np
 
 class Board:
     def __init__(self):
         init_board = [0 for i in range(16)]
         count = 0
         while count < 2:
-            pos = randint(0,15)
+            pos = randint(0, 15)
             if init_board[pos] == 0:
                 ran = random.random()
                 if ran > .9:
@@ -19,26 +19,26 @@ class Board:
                 count += 1
         self.board = [[init_board[i], init_board[i+1], init_board[i+2], init_board[i+3]] for i in range(0, 13, 4)]
 
-    #print state of board
     def print_board(self):
+        """print state of board"""
         print(np.matrix(self.board))
 
-    #generate a new tile on the board
     def generate_piece(self):
+        """generate a new tile on the board after each move"""
         while True:
-            x = randint(0,3)
-            y = randint(0,3)
-            if self.board[x][y] == 0:
+            i = randint(0, 3)
+            j = randint(0, 3)
+            if self.board[i][j] == 0:
                 ran = random.random()
                 if ran > .9:
-                    self.board[x][y] = 4
+                    self.board[i][j] = 4
                 else:
-                    self.board[x][y] = 2
+                    self.board[i][j] = 2
                 break
 
-    #move tile left
+    # move tile left
     def move_left(self):
-        for j in range (len(self.board)):
+        for j in range(len(self.board)):
             row = self.board[j]
             added = set()
             for i in range(3):
@@ -48,7 +48,7 @@ class Board:
                     elif self.board[j][i] == self.board[j][x] and self.board[j][i] != 0 and self.board[j][x] != 0 and (j,i) not in added:
                         self.board[j][i] = self.board[j][i]*2
                         self.board[j][x] = 0
-                        added.add((j,i))
+                        added.add((j, i))
 
             zero = []
             full = []
@@ -104,8 +104,8 @@ class Board:
         if not isSame:
             self.generate_piece()
 
-    #check how many empty spaces are on the board
     def get_empty(self):
+        """checks how many empty spaces are on the board"""
         count = 0
         for x in range(4):
             for y in range(4):
@@ -113,10 +113,10 @@ class Board:
                     count += 1
         return count
 
-    #check if the game has been won/loss
+    # check if the game has been won/loss
     def check_board(self):
-        for i in range(0,4):
-            for j in range(0,4):
+        for i in range(4):
+            for j in range(4):
                 if self.board[i][j] == 0:
                     return False
                 if i > 0:
@@ -144,25 +144,25 @@ class Board:
 
 
 #play the game
-board = Board()
-board.print_board()
-print()
-
-while True:
-    board.print_board()
-    move = getch.getch()
-    if ord(move) == 27:
-        break
-    elif move == 'w':
-        board.move_board('up')
-    elif move == 'a':
-        board.move_board('left')
-    elif move == 's':
-        board.move_board('down')
-    elif move == 'd':
-        board.move_board('right')
-    print()
-    if board.check_board():
-        board.print_board()
-        print("You Lose!")
-        break
+# board = Board()
+# board.print_board()
+# print()
+#
+# while True:
+#     board.print_board()
+#     move = getch.getch()
+#     if ord(move) == 27:
+#         break
+#     elif move == 'w':
+#         board.move_board('up')
+#     elif move == 'a':
+#         board.move_board('left')
+#     elif move == 's':
+#         board.move_board('down')
+#     elif move == 'd':
+#         board.move_board('right')
+#     print()
+#     if board.check_board():
+#         board.print_board()
+#         print("You Lose!")
+#         break
